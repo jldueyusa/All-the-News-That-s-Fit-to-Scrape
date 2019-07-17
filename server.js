0ea1dc691f64aff59d6a39bc2293486786cc3dcf
Jennifer Duey <jlduey@gmail.com>
2019-07-16 20:22:05 -0400

    added dependencies and initialize express

---
+++ server.js
@@ -0,0 +1,8 @@
+//add dependencies
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var logger = require("morgan");
+
+//initialize express app
+var express = require("express");
+var app = express();
