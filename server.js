0acfe79258fb1219d31d4c0796e230cb505b9aef
Jennifer Duey <jlduey@gmail.com>
2019-07-16 20:26:31 -0400

    added logger

--- server.js
+++ server.js
@@ -6,3 +6,11 @@ var logger = require("morgan");
 //initialize express app
 var express = require("express");
 var app = express();
+
+// set up logger for development
+app.use(logger("dev"));
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
